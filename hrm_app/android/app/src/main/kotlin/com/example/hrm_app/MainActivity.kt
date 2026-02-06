535bb9b080f3cd9eca494f388399495eb04b0826
SandSpace Technologies <sandspace.tega@gmail.com>
2026-02-06 14:46:24 +0530

    Delete hrm_app directory

--- hrm_app/android/app/src/main/kotlin/com/example/hrm_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hrm_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- hrm_app/lib/core/constants/app_strings.dart
+++
@@ -1,8 +0,0 @@
-class AppStrings {
-  static const String penaltyLabel = 'Penalty';
-  static const String avgHrsLabel = 'Avg. Hrs'; // Existing usage placeholder
-  static const String lateLabel = 'Late';
-  static const String breakTimeLabel = 'Break Time';
-  static const String punchIn = 'Punch In';
-  static const String punchOut = 'Punch Out';
-}
